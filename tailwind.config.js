5913c1ca645357b5370067b77cc3c31d22f55977
Susk4 <35198313+Susk4@users.noreply.github.com>
2022-12-11 21:54:13 +0100

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
-      "./node_modules/flowbite/**/*.js"
+      "./node_modules/flowbite/**/*.js",
+      "./src/layout/**/*.{js,ts,jsx,tsx}",
 
   ],
   theme: {
