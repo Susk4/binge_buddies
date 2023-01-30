37b87c7c00156cda63733ae8204aa3a974e99a8c
Susk4 <35198313+Susk4@users.noreply.github.com>
2023-01-30 08:08:17 +0100

    image with link component

--- components/User/ProfileData.js
+++ components/User/ProfileData.js
@@ -1,23 +1,17 @@
 import Image from "next/image";
+import UserImage from "../../src/layout/components/UserImage";
 /* import UserStats from "./UserStats"; */
 
 const ProfileData = ({ user }) => {
   return (
     <div className="flex flex-col gap-2">
       <div className="flex items-center justify-center">
-        <Image
-          src={user.photoURL}
-          width="100"
-          height="100"
-          alt="user photo"
-          className="rounded-full"
-        />
+        <UserImage size={100} />
       </div>
       <div>
         <h1 className="text-3xl text-center">{user.displayName}</h1>
       </div>
-      <div>
-      </div>
+      <div></div>
       {/* <UserStats user={user}/> */}
     </div>
   );
--- src/layout/components/Navbar.js
+++ src/layout/components/Navbar.js
@@ -6,7 +6,7 @@ import { IoLogOutOutline } from "react-icons/io5";
 import { BiCameraMovie } from "react-icons/bi";
 import useAuth from "../../hook/useAuth";
 import Link from "next/link";
-import UserImage from "./UserImage";
+import UserImageLink from "./UserImageLink";
 
 export default function Navbar() {
   const { user, logout } = useAuth();
@@ -24,27 +24,26 @@ export default function Navbar() {
       <div className=" text-orange-900 font-bold p-2">
         <div className=" flex h-10 items-center justify-between px-2">
           <div className="flex-none ">
-            <Link href="/" passHref legacyBehavior>
-              <BiCameraMovie className="w-8 h-8 cursor-pointer" />
+            <Link href="/" legacyBehavior>
+              <a>
+                <BiCameraMovie className="w-8 h-8 cursor-pointer" />
+              </a>
             </Link>
           </div>
 
           <NavItems />
 
           <div className="hidden justify-center md:flex flex-none gap-2">
-            {user ? (
-              <div className="flex items-center">
-                {user.photoURL ? <UserImage /> : `User: ${user.displayName}`}
-              </div>
-            ) : (
-              <div>Not logged in</div>
-            )}
+            <div className="flex items-center">
+              <UserImageLink />
+            </div>
+
             <button onClick={logout}>
               <IoLogOutOutline size={size} />
             </button>
           </div>
           <div className="md:hidden flex items-center gap-2">
-            {user.photoURL ? <UserImage /> : `User: ${user.displayName}`}
+            <UserImageLink />
             <button
               onClick={() => {
                 setIsVisible(!isVisible);
@@ -57,7 +56,9 @@ export default function Navbar() {
         <div className={` ${isVisible ? "" : "hidden"} md:hidden`}>
           {mappedItems}
           <div className="px-2 hover:bg-orange-500 ">
-            <button onClick={logout} className="flex gap-2  items-center">Logout <IoLogOutOutline size={size} /></button>
+            <button onClick={logout} className="flex gap-2  items-center">
+              Logout <IoLogOutOutline size={size} />
+            </button>
           </div>
         </div>
       </div>
