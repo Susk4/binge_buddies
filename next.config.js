e50c66bf2864d8d0924d02c476890b5a7a48be3a
Susk4 <35198313+Susk4@users.noreply.github.com>
2023-02-01 16:51:09 +0100

    new flow 10

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com', 'graph.facebook.com'],
