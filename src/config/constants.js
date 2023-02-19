f48678cf8b5e9c907d934faa8165c2d8594d7fb2
Susk4 <35198313+Susk4@users.noreply.github.com>
2023-02-19 21:39:05 +0100

    removed explore page

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const NAV_ITEMS = [
-  { label: "Explore", url: "/explore" },
+  { label: "Explore", url: "/" },
   { label: "My Likes", url: "/likes" },
   { label: "Groups", url: "/groups" },
 ];
