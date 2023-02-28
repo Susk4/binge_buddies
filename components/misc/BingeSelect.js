a2dfcd0b0dcb85085c6a02a9bb58dbdac5633f78
Susk4 <35198313+Susk4@users.noreply.github.com>
2023-02-28 14:14:45 +0100

    select all

--- components/misc/BingeSelect.js
+++ components/misc/BingeSelect.js
@@ -3,10 +3,28 @@ import Select from "react-select";
 import React from "react";
 
 const BingeSelect = (props) => {
+  const optionsWithSelectAll = [
+    { value: "all", label: "Select All" },
+    ...props.options,
+  ];
+  const handleOnChange = (selectedOptions) => {
+    if (selectedOptions.some((option) => option.value === "all")) {
+      props.onChange(
+        optionsWithSelectAll.filter((option) => option.value !== "all")
+      );
+    } else {
+      props.onChange(selectedOptions);
+    }
+  };
+
   return (
     <Select
       isMulti
-      {...props}
+      options={optionsWithSelectAll}
+      isLoading={props.isLoading}
+      isDisabled={props.isDisabled}
+      onChange={handleOnChange}
+      value={props.value}
       menuPlacement="auto"
       className="binge-select-container"
       classNamePrefix="binge-select"
