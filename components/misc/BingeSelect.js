7c40f4a23b20e85a4e7e4d9ffae4d3648a324336
Susk4 <35198313+Susk4@users.noreply.github.com>
2023-02-28 12:50:50 +0100

    remove animation

--- components/misc/BingeSelect.js
+++ components/misc/BingeSelect.js
@@ -1,16 +1,12 @@
 import Select from "react-select";
-import makeAnimated from "react-select/animated";
 
 import React from "react";
 
 const BingeSelect = (props) => {
-  const animatedComponents = makeAnimated();
-
   return (
     <Select
       isMulti
       {...props}
-      components={animatedComponents}
       menuPlacement="auto"
       className="binge-select-container"
       classNamePrefix="binge-select"
