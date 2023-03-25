a65379e1c60d031b3a1cf61cf5eceaecde607d9c
Susk4 <35198313+Susk4@users.noreply.github.com>
2023-03-25 13:53:54 +0100

    navigation updates

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,6 @@
 export const NAV_ITEMS = [
   { label: "Explore", url: "/" },
+  { label: "Movies", url: "/movies" },
   { label: "My Likes", url: "/likes" },
   { label: "Contacts", url: "/contacts" },
   { label: "Profile", url: "/user" },
