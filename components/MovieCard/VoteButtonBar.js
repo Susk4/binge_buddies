22dbb1d6954ee658e1ce9cc6ccbefd7f2190ddca
Susk4 <35198313+Susk4@users.noreply.github.com>
2023-03-25 13:53:27 +0100

    consolidate border-radius property

--- components/MovieCard/VoteButtonBar.js
+++ components/MovieCard/VoteButtonBar.js
@@ -5,12 +5,12 @@ const VoteButtonBar = ({ like, dislike }) => {
   return (
     <div className="flex flex-row justify-between pt-2 w-full">
       <VoteButton
-        className="bg-red-600 p-2 text-black font-bold rounded w-1/4"
+        className="bg-red-600 p-2 text-black font-bold rounded-xl w-1/4"
         text={"Dislike"}
         onTap={dislike}
       />
       <VoteButton
-        className="bg-green-600 p-2 text-black font-bold rounded w-1/4"
+        className="bg-green-600 p-2 text-black font-bold rounded-xl w-1/4"
         text={"Like"}
         onTap={like}
       />
