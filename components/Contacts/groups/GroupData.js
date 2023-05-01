6a064fc138ec93e5c2e1533d074d4ea29d1aa8dc
Susk4 <35198313+Susk4@users.noreply.github.com>
2023-05-01 16:29:51 +0200

    icon label click to trigger onclick

--- components/Contacts/groups/GroupData.js
+++ components/Contacts/groups/GroupData.js
@@ -31,36 +31,36 @@ const GroupData = ({ group, refetch }) => {
       <div className="flex flex-row justify-center items-center gap-2">
         <h2 className="text-xl font-bold text-center">{group.name}</h2>
         {user.uid === group.creator && (
-          <div className="flex text-red-500 hover:text-red-700 cursor-pointer">
-            <HiOutlineTrash
-              className="w-6 h-6"
-              onClick={() => {
-                const data = {
-                  title: "Delete Group",
-                  description: "Are you sure you want to delete this group?",
-                  callback: handleGroupDelete,
-                };
-                setDialogData(data);
-                setIsOpen(true);
-              }}
-            />
+          <div
+            className="flex text-red-500 hover:text-red-700 cursor-pointer"
+            onClick={() => {
+              const data = {
+                title: "Delete Group",
+                description: "Are you sure you want to delete this group?",
+                callback: handleGroupDelete,
+              };
+              setDialogData(data);
+              setIsOpen(true);
+            }}
+          >
+            <HiOutlineTrash className="w-6 h-6" />
             Delete
           </div>
         )}
         {group.users.some((u) => u.id === user.uid && u.accepted) && (
-          <div className="flex text-red-500 hover:text-red-700 cursor-pointer">
-            <IoLogOutOutline
-              className="w-6 h-6 text-red-500 hover:text-red-700 cursor-pointer"
-              onClick={() => {
-                const data = {
-                  title: "Leave Group",
-                  description: "Are you sure you want to leave this group?",
-                  callback: handdleGroupLeave,
-                };
-                setDialogData(data);
-                setIsOpen(true);
-              }}
-            />
+          <div
+            className="flex text-red-500 hover:text-red-700 cursor-pointer"
+            onClick={() => {
+              const data = {
+                title: "Leave Group",
+                description: "Are you sure you want to leave this group?",
+                callback: handdleGroupLeave,
+              };
+              setDialogData(data);
+              setIsOpen(true);
+            }}
+          >
+            <IoLogOutOutline className="w-6 h-6" />
             Leave
           </div>
         )}
